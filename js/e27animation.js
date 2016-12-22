384c46843c7196fa45f5c2c8f16cce7595508705
Thong Do Anh <Thong Do Anh>
2016-12-22 09:29:45 +0700

    Update Nav

--- js/e27animation.js
+++
@@ -1,7 +0,0 @@
-$(window).bind('scroll', function () {
-
-    if ($(window).scrollTop() > 150)
-        $('#secNav').addClass('nav-down');
-    else
-        $('#secNav').removeClass('nav-down');
-});
--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,50 @@ $.getScript("e27animation.js", function(){
    alert("Script loaded but not necessarily executed.");
 
 });
+
+$(document).ready(function(){
+
+/** ===========================================
+    Hide / show the master navigation menu
+============================================ */
+
+  // console.log('Window Height is: ' + $(window).height());
+  // console.log('Document Height is: ' + $(document).height());
+
+  var previousScroll = 0;
+
+  $(window).scroll(function(){
+
+    var currentScroll = $(this).scrollTop();
+
+    /*
+      If the current scroll position is greater than 0 (the top) AND the current scroll position is less than the document height minus the window height (the bottom) run the navigation if/else statement.
+    */
+    if (currentScroll > 0 && currentScroll < $(document).height() - $(window).height()){
+      /*
+        If the current scroll is greater than the previous scroll (i.e we're scrolling down the page), hide the nav.
+      */
+      if (currentScroll > previousScroll){
+        window.setTimeout(hideNav, 0);
+      /*
+        Else we are scrolling up (i.e the previous scroll is greater than the current scroll), so show the nav.
+      */
+      } else {
+        window.setTimeout(showNav, 0);
+      }
+      /* 
+        Set the previous scroll value equal to the current scroll.
+      */
+      previousScroll = currentScroll;
+    }
+
+  });
+
+  function hideNav() {
+    $(".secNav").removeClass("nav-visible").addClass("nav-hidden");
+  }
+  function showNav() {
+    $(".secNav").removeClass("nav-hidden").addClass("nav-visible");
+  }
+
+});
