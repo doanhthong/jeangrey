464ccef3240ed1a924dbdb691fdea5fb0abfd384
Vo Hoai Son <vector.mic@gmail.com>
2017-11-23 03:07:07 +0700

    Main section + countdown timer

---
+++ top100/js/scripts.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function() {
+    function startTimer(nextDay, display, callback) {
+        var timeInterval = setInterval(function() {
+            var now = moment();
+            var diff = moment.duration(nextDay - now);
+            var diffAsSeconds = Math.floor(diff.asSeconds());
+            $(display).html(renderTimebox(diff.days(), 'DAYS') + renderTimebox(diff.hours(), 'HOURS') + renderTimebox(diff.minutes(), 'MINUTES') + renderTimebox(diff.seconds(), 'SECONDS'));
+
+            if (diffAsSeconds <= 0) {
+                clearInterval(timeInterval);
+                callback();
+            }
+        }, 1000);
+    }
+
+    var nextDay = moment('2017/12/10 02:57:59');
+    var display = $('#time-countdown');
+    
+    startTimer(nextDay, display, function() {
+        alert('Go!');
+    });
+
+    function renderTimebox(number, text) {
+        var numberInString = number.toString();
+        numberInString = number < 10 ? '0' + numberInString : numberInString;
+        var digits = numberInString.split("");
+        var html = '';
+        for (var index = 0; index < digits.length; index++) {
+            html += '<span>' + digits[index] + '</span>';
+        }
+        html += '<div>' + text + '</div>';
+        return '<div class="timebox">' + html + '</div>';
+    }
+});
